1140d1f9120fb54120c583d6cef3019e94673fe3
Samyaksam17 <jsamyak407@gmail.com>
2022-12-12 14:54:25 +0530

    API

--- method.js
+++ method.js
@@ -3,36 +3,141 @@ const app = express();
 app.use(express.json());
 
 
-let user = {};
+let user = [
+ {
+    id: 1,
+    name: "samyak",
+    age: 20,
+ } ,
+ {
+    id: 2,
+    name: "sammy",
+    age: 30,
+ } ,
+ {
+    id: 3,
+    name: "samieeeee",
+    age: 40,
+ } ,
 
-app.get('/user', (req,res) => {
+];
+
+const userRouter = express.Router();
+app.use('/user', userRouter);
+
+userRouter
+    .route("/")
+    .get(getUser)
+    .post(postUser)
+    .patch(updateUser)
+    .delete(deleteUser)
+
+userRouter
+    .route("/:name")
+    .get(getUserById);
+
+
+// app.get('/user', (req,res) => {
+//     res.send(user);
+// })
+
+// with query
+// app.get('/user', (req,res) => {
+//     console.log(req.query);
+//     res.send(user);
+// })
+
+// app.post('/user', (req,res) => {
+//     console.log(req.body);
+//     res.json({
+//         message: "Data recieved successfully",
+//         user: req.body
+//     });
+// })
+
+// app.patch('/user', (req,res) => {
+//     console.log(req.body);
+//     let dataToBeUpdated = req.body;
+//     for(key in dataToBeUpdated){
+//         user[key] = dataToBeUpdated[key];
+//     }
+//     res.json({
+//         message:"data updated successfully"
+//     })
+// });
+
+// app.delete('/user', (req,res) => {
+//     user = {};
+//     res.json({
+//         message: "user has been deleted"
+//     })
+// })
+
+
+// // parameters
+
+// app.get('/user/:id', (req,res) => {
+//     // /user/:name
+//     console.log(req.params.id);
+//     // let {id} = req.params;
+//     // let user = db.findOne(id);
+//     res.json({ msg:"user id is ", "obj": req.params });
+// })
+
+
+
+
+
+function getUser(req, res){
+    console.log(req.query);
+    let { name, age } = req.query;
+    // let filteredData=user.filter(userObj => {
+    //     return (userObj.name==name && userObj.age==age)
+    // })
+    // res.send(filteredData);
     res.send(user);
-})
+}
 
-app.post('/user', (req,res) => {
-    console.log(req.body);
+function postUser(req, res){
+    console.log(req.body.Name);
+    //then i can put this in db 
+    user.push(req.body);
     res.json({
-        message: "Data recieved successfully",
+        message: "Data received successfully",
         user: req.body
     });
-})
+}
 
-app.patch('/user', (req,res) => {
+function updateUser(req, res){
     console.log(req.body);
     let dataToBeUpdated = req.body;
-    for(key in dataToBeUpdated){
+    for (key in dataToBeUpdated) {
         user[key] = dataToBeUpdated[key];
     }
     res.json({
-        message:"data updated successfully"
+        message: "data updated succesfully"
     })
-});
+}
 
-app.delete('/user', (req,res) => {
+function deleteUser(req, res){
     user = {};
     res.json({
-        message: "user has been deleted"
-    })
-})
+        msg: "user has been deleted"
+    });
+}
+
+function getUserById(req, res){
+    console.log(req.params.name);
+    //let {id}=req.params;
+    // let user = db.findOne(id);
+    res.json({ msg: "user id is ", "obj": req.params });
+}
+
+
+
+
+
+
+
 
 app.listen(5500);
